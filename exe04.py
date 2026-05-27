fb97eacaa1f59bce1ab40afaa7bcd514a9a590cd
Seu Nome <carlos.jrconstantino@gmail.com>
2026-05-27 20:00:25 -0300

    exe04

---
+++ exe04.py
@@ -0,0 +1,13 @@
+def maior_numero(a, b):
+    if a > b:
+        print(f"O número {a} é maior")
+    else: 
+        print(f"O número {b} é maior")
+
+#Exemplo: 
+
+maior_numero (a=3, b=9)
+
+#Pode ser assim também: 
+#maior_numero (3, 9)
+
