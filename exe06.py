fc0c0ce2136fc311570cd2125f80204d3f967284
Seu Nome <carlos.jrconstantino@gmail.com>
2026-05-27 21:16:18 -0300

    exe06

---
+++ exe06.py
@@ -0,0 +1,15 @@
+for i in "BRUNO":
+    if i in "AEIOU":
+        print(i)
+
+
+#Ou assim:
+
+def contar_vogais(texto):
+    count_vogal = 0
+    for letra in texto:
+        if letra.upper() in "AEIOU":
+            count_vogal += 1
+    return count_vogal
+
+print(contar_vogais("BRUNO"))
